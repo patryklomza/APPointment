9d1534daeb986ec8d35db0d6eabd2d7eac9e5bbe
Patryk Lomza <p.lomza@outlook.com>
2019-06-10 19:02:44 +0200

    Add receiving login credentials in login route

--- appointment/app/routes.py
+++ appointment/app/routes.py
@@ -1,13 +1,18 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, \
+            remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
