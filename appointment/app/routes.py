c5d1d39379bcabc309fcfec1d1c4649f11be7e47
Patryk Lomza <p.lomza@outlook.com>
2019-06-10 18:42:46 +0200

    Add login route

--- appointment/app/routes.py
+++ appointment/app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
