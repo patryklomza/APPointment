392c0bae52cd4a6cd5160f18fe933e54fedc2972
Patryk Lomza <p.lomza@outlook.com>
2019-06-10 19:37:41 +0200

    Add visit form

--- appointment/app/forms.py
+++ appointment/app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,  PasswordField, BooleanField, SubmitField
+from wtforms import StringField,  PasswordField, BooleanField, SubmitField, DateField, TimeField
 from wtforms.validators import DataRequired
+
 
 class LoginForm(FlaskForm):
     username = StringField('Nazwa użytkownika', validators=[DataRequired(message="Pole wymagane")])
     password = PasswordField('Hasło', validators=[DataRequired(message="Pole wymagane")])
     remember_me = BooleanField('Zapamiętaj mnie')
     submit = SubmitField('Zaloguj')
+
+
+class VisitForm(FlaskForm):
+    visit_date = DateField('Data nowej wizyty',render_kw={"type":"date"})
+    visit_time = TimeField('Godzina nowej wizyty',render_kw={"type":"time"})
+    submit = SubmitField('Wyślij')
