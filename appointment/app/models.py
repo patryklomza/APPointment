f808fa7f8b4c02f22b920d41922eccd2061527bd
Patryk Lomza <p.lomza@outlook.com>
2019-06-11 09:12:34 +0200

    Add flask-login user loader function

--- appointment/app/models.py
+++ appointment/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask_login import UserMixin
+from app import login
 
 
 class User(UserMixin, db.Model):
@@ -10,9 +11,14 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
