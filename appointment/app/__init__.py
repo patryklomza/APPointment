0d16aa9cb3e6fc6ce5c2c649b5eac16cff6ed997
Patryk Lomza <p.lomza@outlook.com>
2019-06-12 14:16:02 +0200

    Fix typo
    Fixed typo in bootstrap initialization

--- appointment/app/__init__.py
+++ appointment/app/__init__.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-bootstrap = Bootstrap
+bootstrap = Bootstrap(app)
 
 
 from app import routes, models
