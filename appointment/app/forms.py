29a25bde349be7d6e4b2020e86be27e10ac4e363
Patryk Lomza <p.lomza@outlook.com>
2019-06-15 13:18:34 +0200

    Delete redundant comment

--- appointment/app/forms.py
+++ appointment/app/forms.py
@@ -25,7 +25,6 @@ class VisitForm(FlaskForm):
                ('18:00', '18:00'), ('18:30', '18:30')]
 
     visit_date = DateField('Data nowej wizyty',render_kw={"type":"date"})
-   # visit_time = TimeField('Godzina nowej wizyty',render_kw={"type":"time"})
     visit_time = SelectField('Godzina nowej wizyty', choices=choices)
     submit = SubmitField('Wyślij')
 
