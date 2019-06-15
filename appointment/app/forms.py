ca6e6298712ac644bfe1a244a514ba393dd962a0
Patryk Lomza <p.lomza@outlook.com>
2019-06-15 13:16:23 +0200

    Delete unused imports

--- appointment/app/forms.py
+++ appointment/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,  PasswordField, BooleanField, SubmitField, DateField, TimeField, SelectField
+from wtforms import StringField,  PasswordField, BooleanField, SubmitField, DateField, SelectField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
 
