41435079b208d8c957ee12a428ac3e02b8bf3266
Patryk Lomza <p.lomza@outlook.com>
2019-07-28 20:29:43 +0200

    Add template and shell context processors

--- appointment/app/main/__init__.py
+++ appointment/app/main/__init__.py
@@ -1,5 +1,13 @@
 from flask import Blueprint
 
+from app.models import Permission
+
 main = Blueprint('main', __name__)
+
+# context processor adds Permission class to the template context
+# this lets avoid  having to add a template argument in every render_template() call
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
 
 from . import views
--- appointment/appointment.py
+++ appointment/appointment.py
@@ -1,7 +1,12 @@
-from app import create_app, Config
+from app import create_app, Config, db
+
+from app.models import User, Role, Visit, ScheduleTime, Permission
 
 
 app = create_app(Config)
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role, Visit=Visit, ScheduleTime=ScheduleTime, Permission=Permission)
 
 if __name__ == "__main__":
     app.run(debug=True)
