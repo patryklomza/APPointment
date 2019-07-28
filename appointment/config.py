75da8c792970ff6e4510148e58ffdcaee9aa006d
Patryk Lomza <p.lomza@outlook.com>
2019-07-28 20:31:07 +0200

    Add admin constant to config

--- appointment/config.py
+++ appointment/config.py
@@ -6,3 +6,4 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APPOINTMENT_ADMIN = os.environ.get('APPOINTMENT_ADMIN')
