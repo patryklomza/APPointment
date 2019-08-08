8c2ec55ecdbdd922efdd945c78897e47e1902fce
Patryk Lomza <p.lomza@outlook.com>
2019-08-08 12:01:10 +0200

    Add page for all visits in admin view

--- appointment/app/admin/views.py
+++ appointment/app/admin/views.py
@@ -1,7 +1,7 @@
-from . import admin
-from app.decorators import admin_required
 from flask import render_template
-
+from app.decorators import admin_required
+from app.models import Visit, User
+from . import admin
 
 # all routes for admin are accessed with /admin prefix
 
@@ -9,3 +9,9 @@ from flask import render_template
 @admin_required
 def admin_dashboard():
     return render_template('admin/admin_dashboard.html')
+
+@admin.route('/visits')
+@admin_required
+def customer_visits():
+    all_visits = Visit.query.join(Visit.customer).filter_by(role_id=1)
+    return render_template('admin/all_visits.html', all_visits=all_visits)
