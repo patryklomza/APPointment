7b15ba1a96e4a9cace3d4fc46bf409a7c042fbc2
Patryk Lomza <p.lomza@outlook.com>
2020-03-12 19:02:31 +0100

    Add host to app run configuration

--- appointment/appointment.py
+++ appointment/appointment.py
@@ -9,4 +9,4 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Visit=Visit, ScheduleTime=ScheduleTime, Permission=Permission)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
